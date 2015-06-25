012cb582a8a9a08e6175a6f7ef7f821f15aa6388
Daz Ahern <darren.ahern@gmail.com>
2015-06-25 11:13:44 +0100

    Loop over watches array

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,5 +1,15 @@
 class WatchesController < ApplicationController
   def index
+  	#PAss an instance variable to the view
+
+  	@watches = [
+  		{name: "Backup Black", brand: "Swatch"}, 
+  		{name: "Classical Digital Casio", brand: "Casio"}, 
+  		{name: "1960s Vintage Raketa", brand: "Raketa"}, 
+  		{name: "Max Bill Chronoscope", brand: "Junghans"}
+  	]
+
+  	#Rails is rendering the index view
   end
 
   def show
